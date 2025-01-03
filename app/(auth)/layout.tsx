40c7ab7be55399a5fe153ac1d48cacac4b83870b
LEO2022110011 <108049038+LEO2022110011@users.noreply.github.com>
2025-01-03 22:14:08 +0700

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className="flex min-h-screen w-full justify-between font-inter">
+      {children}
+      <div className="auth-asset">
+        <div>
+        </div>
+      </div>
+    </main>
+  );
+}
